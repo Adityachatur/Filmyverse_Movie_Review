01a9fccde31bf561363d150f8cddb7ec7f1b4f63
Adityachatur <66326241+Adityachatur@users.noreply.github.com>
2023-11-16 23:23:39 +0530

    new changes

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -8,9 +8,10 @@ const Header = () => {
   const useAppState = useContext(AppState);
 
   return (
-    <div>
-      <div className='text-3xl  flex justify-between items-center font-bold p-3 border-b-2 border-gray-500 text-green-500 header'>
-        <Link to={'/'}>
+    <div className="sticky top-0 z-50">
+      <div className='relative text-3xl flex justify-between items-center font-bold p-3 border-b-2 border-gray-100 text-green-500 header' >
+        <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
+        <Link to={'/'} className="relative z-10">
           <span>
             Filmy <span className='text-white'>Verse</span>
           </span>
@@ -18,25 +19,20 @@ const Header = () => {
 
         {useAppState.login ? (
           <div className='flex justify-end'>
-          <Link to={'/Addmovie'}>
-            <h3 className='text-2xl'>
-              <Button>
-                <AddIcon className='mr-2  text-3xl flex items-center' color='secondary' />
-                <span className='text-white font-bold text-xl'>Add New</span>
-              </Button>
-             
-            </h3>
-          </Link>
-          {/* <Button className='bg-green-700 text-2xl font-bold p-2'>
-            Logout
-          </Button> */}
+            <Link to={'/Addmovie'}>
+              <h3 className='text-2xl'>
+                <Button>
+                  <AddIcon className='mr-2  text-3xl flex items-center' color='secondary' />
+                  <span className='text-white font-bold text-xl'>Add New</span>
+                </Button>
+              </h3>
+            </Link>
           </div>
-          
         ) : (
           <Link to={'/Login'}>
             <h3 className='text-2xl'>
               <Button>
-                <span className='text-white bg-green-800 p-2 text-2xl rounded-lg hover:bg-gray-800'>Login</span>
+                <span className='text-white bg-green-800 p-2 text-xl rounded-lg hover:bg-gray-800'>Login</span>
               </Button>
             </h3>
           </Link>
